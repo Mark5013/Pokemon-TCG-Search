753f2745d6453c0e5b245a9d6fb958906c70d0cc
Mark5013 <70342735+Mark5013@users.noreply.github.com>
2022-06-03 17:04:26 -0400

    add pagination to site

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import pokemon from 'pokemontcgsdk';
-
+import paginate from 'paginate';
 
 const app = express();
 const port = 3000;
 pokemon.configure({apiKey: `${process.env.apiKey}`})
 
+//used to store most recent search from user
+let curPokemon ="";
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
+
 
 // render home page
 app.get("/", (req,res) => {
@@ -18,43 +22,77 @@ app.get("/", (req,res) => {
 });
 
 // handles searches by pokemon name
-app.post("/search", (req, res) => {
-    let pokemonName = req.body.searchBar;
+app.post("/search/:page", (req, res) => {
+    // searchEntry from user
+    let searchEntry = req.body.searchBar;
+
+    // This happens when user inputs something into search bar, store it in global variable for reference later
+    if(searchEntry !== undefined) {
+        curPokemon = searchEntry;
+    }
+
+    // Starting index for query results
+    let startIndex = 0;
+
+    // This happens user clicks on button to go to next page, so store last search in searchEntry
+    if(searchEntry === undefined) {
+        searchEntry = curPokemon;
+    }
+
+    // If body has this property, then user clicks on button, find what page they want and set proper start index
+    if(req.body.hasOwnProperty("nextBtn")) {
+        startIndex = (req.body.nextBtn - 1) * 24;
+    }
+
+    // Update end index to 24 after start index
+    let endIndex = startIndex + 24;
 
     //MAKRE SURE USER INPUT DOESN'T CRASH API QUERY
-    if(!(/^[A-Za-z0-9-]*$/.test(pokemonName))) { 
+    if(!(/^[A-Za-z0-9-]*$/.test(searchEntry))) { 
+        //Redirect to failure page based off of invalid input
         res.redirect("failure");
-    } else if(pokemonName.includes('-')) {
-        //HANDLES SEARCH BY ID
-        pokemon.card.where({ q: `id:${pokemonName}`}).then(
-            result => {
-                //check if pokemon was found
-                let dataLength = result.data.length;
-                if(dataLength === 0) {
-                    res.render("failure");
-                    res.end();
-                } else {
-                    res.redirect(`/cardpage/${pokemonName}`);
-                }
-            }
-        );
-    } else  {
+        //Check if search entry is undefined or doesn't include - symbol
+    } else if(searchEntry === undefined || !searchEntry.includes('-')) {
         // HANDLES SEARCH BY NAME
-        pokemon.card.where({ q: `name:${pokemonName}`}).then(
+        pokemon.card.where({ q: `name:${searchEntry}`}).then(
             result => {
                 // check if pokemon was found
-                let dataLength = result.data.length;
-                if(dataLength === 0) {
+                let numOfResults = result.data.length;
+                // make sure end index doesn't go out of bounds of arr returned
+                if(endIndex > numOfResults) {endIndex = numOfResults;}
+                // render failure page if no results found
+                if(numOfResults === 0) {
                     res.render("failure");
                     res.end();
+                //render page based off of search results
                 } else {
                     res.render("search", {
                         cardArr: result.data,
+                        numOfResults: numOfResults,
+                        startIndex: startIndex,
+                        endIndex: endIndex,
+                        activeBtn: req.body.nextBtn,
                     });
                     res.end();
                 }
             }
         );
+    } else  {
+        //HANDLES SEARCH BY ID
+        pokemon.card.where({ q: `id:${searchEntry}`}).then(
+        result => {
+            //check if pokemon was found
+            let numOfResults = result.data.length;
+            // render failure page if no results found
+            if(numOfResults === 0) {
+                res.render("failure");
+                res.end();
+                //render card info page for id
+            } else {
+                res.redirect(`/cardpage/${searchEntry}}`);
+            }
+        }
+);
     }
 })
 
@@ -64,21 +102,25 @@ app.get("/cardpage/:cardID", (req, res) => {
 
     pokemon.card.where({ q: `id:${pokemonId}`}).then(
         result => {
-            res.render('cardpage', {
-                pokemonImg: result.data[0].images.large,
-                pokemonName: result.data[0].name,
-                pokemonHP: result.data[0], 
-                pokemonTypes: result.data[0],
-                pokemonFrom: result.data[0],
-                pokemonEvos: result.data[0],
-                pokemonWeak: result.data[0],
-                pokemonRarity: result.data[0].rarity,
-                tcgLink: result.data[0].tcgplayer.url,
-                cardMarket: result.data[0].cardmarket.url,
-                cardArtist: result.data[0].artist,
-                cardId: pokemonId,
-            });
-            res.end();
+            if(result.data.length === 0) {
+                res.render("failure")
+            } else {
+                res.render('cardpage', {
+                    pokemonImg: result.data[0].images.large,
+                    pokemonName: result.data[0].name,
+                    pokemonHP: result.data[0], 
+                    pokemonTypes: result.data[0],
+                    pokemonFrom: result.data[0],
+                    pokemonEvos: result.data[0],
+                    pokemonWeak: result.data[0],
+                    pokemonRarity: result.data[0].rarity,
+                    tcgLink: result.data[0].tcgplayer.url,
+                    cardMarket: result.data[0].cardmarket.url,
+                    cardArtist: result.data[0].artist,
+                    cardId: pokemonId,
+                });
+                res.end();
+            }
         }
     )
 
--- public/javascript/script.js
+++ public/javascript/script.js
@@ -3,4 +3,8 @@ let today = new Date();
 let imgArr = ["/images/dratini.png", "/images/eevee.png", "/images/lechonk.png", "/images/munchlax.png", "/images/pachirisu.png",
                 "/images/piplup.png", "/images/turtwig.png"];
 
-document.querySelector(".home-pg-img").src = imgArr[today.getDay()];
+const img = document.querySelector(".home-pg-img");
+if(img !== null) {
+    img.src = imgArr[today.getDay()];
+}
+

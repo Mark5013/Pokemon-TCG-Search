220e3ec6ae40d338355312691ea58ac4b75947c3
Mark5013 <70342735+Mark5013@users.noreply.github.com>
2022-06-06 20:27:21 -0400

    ADD GOOGLE OAUTH20 LINK WITH MONGODB/MONGOOSE AND COLLECTION PAGE

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
+import dotenv from 'dotenv'
+dotenv.config();
 import express from 'express';
 import bodyParser from 'body-parser';
 import pokemon from 'pokemontcgsdk';
+import mongoose from 'mongoose';
+import session from 'express-session';
+import passport from 'passport';
+import passportLocalMongose from 'passport-local-mongoose';
+import GoogleStrategy from 'passport-google-oauth20';
+GoogleStrategy.Strategy;
+import findOrCreate from 'mongoose-findorcreate';
 
 const app = express();
 const port = 3000;
-pokemon.configure({apiKey: `${process.env.apiKey}`})
+pokemon.configure({apiKey: `${process.env.API_KEY}`})
 
 //used to store most recent search from user
 let curPokemon ="";
@@ -13,15 +22,125 @@ let latestStartIndex;
 let latestActiveBtn;
 let curStyle = "images";
 
+const password = process.env.PASS_KEY;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
+// config session pkg
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+
+// init passport package
+app.use(passport.initialize());
+// tell pasport to deal with sessions
+app.use(passport.session());
+
+mongoose.connect(`mongodb+srv://admin-mark:${password}@cluster0.vgavw.mongodb.net/collectionDB`);
+
+const  collectionSchema = new mongoose.Schema({
+    googleId: String,
+    cards: [{cardId: String, cardImg: String}]
+});
+
+// hash and salt password and save in DB
+collectionSchema.plugin(passportLocalMongose);
+collectionSchema.plugin(findOrCreate);
+
+const Collection = new mongoose.model("Collection", collectionSchema);
+//create login strategy
+passport.use(Collection.createStrategy());
+
+passport.serializeUser(function(user, cb) {
+    process.nextTick(function() {
+        cb(null, { id: user.id, username: user.username, name: user.name });
+    });
+});
+  
+passport.deserializeUser(function(user, cb) {
+    process.nextTick(function() {
+        return cb(null, user);
+    });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "https://whispering-retreat-18765.herokuapp.com/auth/google/pokemon"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    Collection.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 // render home page
 app.get("/", (req,res) => {
-    res.render("index");
-    res.end();
+    console.log(req.session);
+    if(req.session.hasOwnProperty("passport")) {
+        res.render("index", {loggedIn: true});
+    } else {
+        res.render("index", {loggedIn: false});
+    }
+    
+});
+
+app.get("/auth/google", passport.authenticate('google', {
+    scope: ['profile']
+}));
+
+app.get('/auth/google/pokemon', 
+  passport.authenticate('google', { failureRedirect: '/index' }),
+  function(req, res) {
+    // Successful authentication, redirect secrets.
+    res.redirect('/');
+    
+});
+
+app.get("/collection", (req, res) => {
+    if(req.isAuthenticated()) {
+        Collection.findById(req.user.id, function(err, doc) {
+            if(err) {
+                console.log(err);
+            } else {
+                res.render("collection", {cardArr: doc.cards});
+            }
+        })
+    } else {
+        res.redirect("/");
+    }
+});
+
+app.get("/logout", (req, res) => {
+    req.logout(function(err) {
+        if(err) {
+            console.log(err);
+        } else {
+            res.redirect("/");
+        }
+    });
+});
+
+app.post("/addToCollection", (req, res) => {
+    if(req.isAuthenticated()) {
+        let cardId = req.body.addBtn;
+        let cardImg = req.body.cardImg;
+        const userId = req.user.id;
+        Collection.findByIdAndUpdate(userId, {$push: {"cards":{cardId: cardId, cardImg: cardImg}}}, {new: true}, function(err, doc) {
+            if(err) {
+                console.log(err);
+            } else {
+                console.log(doc);
+                res.render("collection", {cardArr: doc.cards});     
+            }
+        });
+    } else {
+        res.redirect("/");
+    }
 });
 
 // handles searches by pokemon name
@@ -231,3 +350,6 @@ app.listen(process.env.PORT || port, (req, res) => {
 });
 
 
+function helper(id) {
+
+}
